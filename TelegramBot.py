cb0e466cec5d84e178c484bcaae291a62cc2ff74
Ulana787 <poplavskaaulana08@gmail.com>
2025-01-17 18:13:59 +0300

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,10 +1,10 @@
-# pip install aiogram=="2.25.1"
 from aiogram import types, Dispatcher, Bot
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup 
 import random
+from keep_alive import keep_alive
+keep_alive()
 
-#bot = Bot(token='5631699730:AAH0LLLo-C9d-WaOa_HgVjQHYhUvtVME9Wg')
 bot = Bot(token='7778561726:AAE2IpeSc2clhpqDfCk-x-e9LosjsoPcJBI')
 dp = Dispatcher(bot)
 
